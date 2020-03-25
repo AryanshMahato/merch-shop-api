06a9904e4aaa9efacfb9f39a6d7da47f62239c82
Aryansh Mahato <aryanshmahato@gmail.com>
2020-03-25 20:49:21 +0530

    Validation Error Optimised

---
+++ src/Error/validationError.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
+
+const validationError = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Request body is not correct",
+      error: errors.array()
+    });
+  }
+  next();
+};
+
+export default validationError;
--- src/Routes/auth.ts
+++ src/Routes/auth.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { signOut, signUp } from "../Controllers/auth";
 import { check } from "express-validator";
+import validationError from "../Error/validationError";
 const authRoutes = Router();
 
 authRoutes.post(
@@ -8,7 +9,8 @@ authRoutes.post(
   [
     check("email", "Name is too shot").isLength({ min: 3 }),
     check("password", "Password is too short").isLength({ min: 6 }),
-    check("email", "Email is invalid").isEmail()
+    check("email", "Email is invalid").isEmail(),
+    validationError
   ],
   signUp
 );
