f82f84ddba216ff2c1e26d51011425d27dd38ecd
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-02 00:13:44 +0530

    Validation added in Category

--- src/Routes/category.ts
+++ src/Routes/category.ts
@@ -17,7 +17,9 @@ const categoryRoutes = Router();
 categoryRoutes.post(
   "/category/create/",
   [
-    check("name", "Name is not provided in body").isEmpty(),
+    check("name", "Name is too short, should be at least 4 char").isLength({
+      min: 3
+    }),
     sendValidationError
   ],
   isSignedIn,
@@ -31,6 +33,12 @@ categoryRoutes.get("/category/:id", isSignedIn, isAdmin, getCategoryById);
 
 categoryRoutes.put(
   "/category/:id",
+  [
+    check("name", "Name is too short, should be at least 4 char").isLength({
+      min: 3
+    }),
+    sendValidationError
+  ],
   isSignedIn,
   isAdmin,
   setCategoryInRequest,
