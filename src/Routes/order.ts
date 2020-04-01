c4ffb52af879687b7528ff0cad878be6dc2532ed
Aryansh Mahato <aryanshmahato@gmail.com>
2020-04-02 00:18:58 +0530

    Validation added in Order

--- src/Routes/order.ts
+++ src/Routes/order.ts
@@ -5,11 +5,26 @@ import {
   getOrderById,
   setOrderInRequest
 } from "../Controllers/order";
+import sendValidationError from "../Errors/sendValidationError";
+import { check } from "express-validator";
 
 const orderRoutes = Router();
 
 orderRoutes.get("/order/:orderId", isSignedIn, setOrderInRequest, getOrderById);
 
-orderRoutes.post("/order/create", isSignedIn, createOrder);
+orderRoutes.post(
+  "/order/create",
+  [
+    check("address", "Address is too short").isLength({
+      min: 10
+    }),
+    check("amount", "Amount is invalid").isNumeric(),
+    check("products", "Product is invalid").isArray(),
+    check("transactionId", "Transaction Id is invalid").isString(),
+    sendValidationError
+  ],
+  isSignedIn,
+  createOrder
+);
 
 export default orderRoutes;
